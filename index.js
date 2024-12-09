1b875a78db1b57505075c7eacd24c2ee895d6489
GracyMa <ma.tianyi@northeastern.edu>
2024-12-08 23:50:44 -0800

    change cors

--- index.js
+++ index.js
@@ -1,63 +1,4 @@
-// import express from "express";
-// import Hello from "./Hello.js";
-// import Lab5 from "./Lab5/index.js";
-// import cors from "cors";
-// import UserRoutes from "./Kanbas/Users/routes.js";
-// import CourseRoutes from "./Kanbas/Courses/routes.js";
-// import session from "express-session";
-// import "dotenv/config";
-// import ModuleRoutes from "./Kanbas/Modules/routes.js";
-// import AssignmentRoutes from "./Kanbas/Assignment/routes.js";
-// import EnrollmentRoutes from "./Kanbas/Enrollments/routes.js";
-// import mongoose from "mongoose";
-// import "dotenv/config";
 
-
-// const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
-// mongoose.connect(CONNECTION_STRING);
-
-// const app = express();
-
-// app.use(
-
-//     cors({
-//         credentials: true,
-//         origin: process.env.NETLIFY_URL || "http://localhost:3000",
-//     })
-// );
-
-// const sessionOptions = {
-    
-//     secret: "any string",
-//     resave: false,
-//     saveUninitialized: false,
-// };
-
-// if (process.env.NODE_ENV !== "development") {
-    
-//     sessionOptions.proxy = true;
-//     sessionOptions.cookie = {
-//       sameSite: "none",
-//       secure: true,
-//       domain: process.env.NODE_SERVER_DOMAIN,
-//     };
-// }
-  
-// app.use(express.json());
-
-// app.use(
-//     session(sessionOptions)
-// );
-
-// UserRoutes(app);
-// CourseRoutes(app);
-// ModuleRoutes(app);
-// AssignmentRoutes(app);
-// EnrollmentRoutes(app);
-// Lab5(app);
-// Hello(app);
-
-// app.listen(process.env.PORT || 4000);
 import express from "express";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
@@ -99,12 +40,14 @@ if (process.env.NODE_ENV !== "development") {
       domain: process.env.NODE_SERVER_DOMAIN,
     };
 }
-  
-app.use(express.json());
 
-app.use(
+  app.use(
     session(sessionOptions)
 );
+
+app.use(express.json());
+
+
 
 UserRoutes(app);
 CourseRoutes(app);
